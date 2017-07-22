6140c4ea58b3f11e987f573ccca4820136eb4a41
cipor <a.cipor@free.fr>
2017-07-22 19:09:15 +0200

    change with array

--- etape1_citations.js
+++ etape1_citations.js
@@ -1,41 +1,34 @@
-var debut = ["Quelle que soit l'orientation présente", "Malgré cette rigueur générale", "Compte tenu de la dualité de la situation présente","En ce qui concerne l'inertie induite"];
-var milieu = ["il convient de comprendre toutes les issues imaginables", "on ne peut se passer de façonner chacune des actions s'offrant à nous", "il faut de toute urgence avoir à l'esprit toutes les décisions évidentes"];
-var fin = ["dans une perspective correcte.","parce qu'il s'agit de notre dernière chance.", "en prenant toutes les précautions qui s'imposent."];
+var typeGenerateur = [
+    [
+    "Quelle que soit l'orientation présente", 
+    "Malgré cette rigueur générale", 
+    "Compte tenu de la dualité de la situation présente",
+    "En ce qui concerne l'inertie induite"],
+	[
+    "il convient de comprendre toutes les issues imaginables",
+    "on ne peut se passer de façonner chacune des actions s'offrant à nous",
+    "il faut de toute urgence avoir à l'esprit toutes les décisions évidentes"
+    ],
+    [
+    "dans une perspective correcte.",
+    "parce qu'il s'agit de notre dernière chance.",
+    "en prenant toutes les précautions qui s'imposent.",
+    "depuis longtemps."
+    ] 
+]; // fin typeGenerateur
 
-var citation = {
-    init: function (debut, milieu, fin) { 
-     this.debut = debut;
-     this.milieu = milieu;
-     this.fin = fin;
-   },
-    decrire: function () { 
-         return (this.debut + " " + this.milieu+ " " +this.fin);
-   }
- };
-
-// calcul un indice aléatoire
-function calcul_index (chaine) {
-    return Math.floor(Math.random() * chaine.length);                      
+// retourne un indice du tableau au hasard
+function random(tableau) {
+    return Math.floor(Math.random() * tableau.length);   
 }
 
-
-// génère 1a citation dans  un tableau 
-function generate_citation(){
-       //on créé un tableau citations
-       var citations = []; 
- 	   // création de l'objet Citation
-       var nouvCitation = Object.create(citation); 
-       // initialise l' Objet nouvCitation avec un début, un milieu et une fin
-       nouvCitation.init(debut[calcul_index(debut)],milieu[calcul_index(milieu)] ,fin[calcul_index(fin)] ); 
-       // ajout dans le tableau
-       citations.push(nouvCitation);
-return citations; // retourne le tableau  citations
- }
-
-// générer la citations dans un tableau
-var tableau_citation = [];
-tableau_citation = generate_citation(); 
-
-// affichage de la  citation
-console.log("Voici la citation : "); 
-console.log(tableau_citation[0].decrire()); 
+// générer la  citation
+var phrase1 = "";
+var position1;
+// 3 éléments dans le tableau
+for ( var i = 0 ;i < 3 ; i++) {
+                                    position1 = random (typeGenerateur[i]);    
+                                    phrase1 =  phrase1 + " " + typeGenerateur[i][position1];
+}
+console.log("\nVoici la citation générée: ");
+console.log(phrase1);
--- etape2_citations.js
+++ etape2_citations.js
@@ -1,61 +1,51 @@
-// 1ere phrase génerateur
-var debut1 = ["Quelle que soit l'orientation présente", "Malgré cette rigueur générale", "Compte tenu de la dualité de la situation présente","En ce qui concerne l'inertie induite"];    
-var milieu1 = ["il convient de comprendre toutes les issues imaginables", "on ne peut se passer de façonner chacune des actions s'offrant à nous", "il faut de toute urgence avoir à l'esprit toutes les décisions évidentes"];
-var fin1 = ["dans une perspective correcte.","parce qu'il s'agit de notre dernière chance.", "en prenant toutes les précautions qui s'imposent.","depuis longtemps."];
+var typeGenerateur = [
+    // generateur 1
+    [ 
+    [
+    "Quelle que soit l'orientation présente", 
+    "Malgré cette rigueur générale", 
+    "Compte tenu de la dualité de la situation présente",
+    "En ce qui concerne l'inertie induite"],
+	[
+    "il convient de comprendre toutes les issues imaginables",
+    "on ne peut se passer de façonner chacune des actions s'offrant à nous",
+    "il faut de toute urgence avoir à l'esprit toutes les décisions évidentes"
+    ],
+    [
+    "dans une perspective correcte.",
+    "parce qu'il s'agit de notre dernière chance.",
+    "en prenant toutes les précautions qui s'imposent.",
+    "depuis longtemps."
+    ]
+    ], // fin generateur 1
+    // generateur 2
+    [ 
+    [
+    "Dans le cas particulier de la baisse de confiance générale",
+    "Tant que durera la morosité conjoncturelle",
+    "Dans le but de pallier à la baisse de confiance contextuelle",
+    "Dans le cas particulier de l'austérité observée"
+    ],
+    [
+    "il convient d'étudier toutes les solutions éventuelles",
+     "il est préférable de remodeler l'ensemble des problématiques s'offrant à nous",
+     "il est très important d'analyser la globalité des actions pertinentes"
+    ],
+    [
+    "à long terme.",
+    "à court terme.", 
+    "parce qu'il est temps d'agir.",
+    "à l'avenir."
+    ]
+    ] // fin generateur 2
+]; // fin typeGenerateur
 
-// 2eme phrase génerateur
-var debut2 = ["Dans le cas particulier de la baisse de confiance générale", "Tant que durera la morosité conjoncturelle", "Dans le but de pallier à la baisse de confiance contextuelle","Dans le cas particulier de l'austérité observée"];
-var milieu2 = ["il convient d'étudier toutes les solutions éventuelles", "il est préférable de remodeler l'ensemble des problématiques s'offrant à nous", "il est très important d'analyser la globalité des actions pertinentes"];
-var fin2 = ["à long terme.","à court terme.", "parce qu'il est temps d'agir.","à l'avenir."];
-
-// objet citation
-var citation = {
-    init: function (debut, milieu, fin) { 
-     this.debut = debut;
-     this.milieu = milieu;
-     this.fin = fin;
-   },
-    decrire: function () { 
-         return (this.debut + " " + this.milieu+ " " +this.fin);
-   }
- };
-
-// calcul un indice aléatoire
-function calcul_index (chaine) {
-    return Math.floor(Math.random() * chaine.length);                      
+// retourne un indice du tableau au hasard
+function random(tableau) {
+    return Math.floor(Math.random() * tableau.length);   
 }
 
- // génère nb citations dans  un tableau avec le type de genrateur de  phrase numPhrase
- function generate_citation(nb,numPhrase){
-     // choix de type générateur
-     var debGen = [];
-     var milGen = [];
-     var finGen = [];
-     if (numPhrase == 1) {
-         debGen = Object.create(debut1);
-         milGen = Object.create(milieu1);
-         finGen = Object.create(fin1);
-     }
-     else {
-         debGen = Object.create(debut2);
-         milGen = Object.create(milieu2);
-         finGen = Object.create(fin2);
-     }
-    //on créé un tableau citations
-    var citations = []; 
- 	for (var i = 0; i < nb; i++) { 
- 		// création de l'objet Citation
-        var nouvCitation = Object.create(citation); 
-        // initialise l' Objet nouvCitation avec un début, un milieu et une fin aléatoire
- 		nouvCitation.init(debGen[calcul_index(debGen)],milGen[calcul_index(milGen)],finGen[calcul_index(finGen)]); 
-        // ajout dans le tableau
- 		citations.push(nouvCitation);
- 	}
- 	return citations; // retourne le tableau  citations
- }
-
-
-// saisie nbre de citations et la phrase choisie
+// saisie nbre de citations et le numero du type generateur
 var nbCitations;
 while ( nbCitations !== 0 ) {
 nbCitations = Number(prompt ("Entrer le nombre de citations à générer de 1 à 5 ( 0 pour finir ):"));
@@ -69,28 +59,31 @@ switch(nbCitations)    {
     case 4:
     case 5:
     // saisie du numero type generateur de citations  
-       var numeroPhrase = 0;
-       while (numeroPhrase != 1 && numeroPhrase != 2) {
-           numeroPhrase= Number(prompt("Entrer le numéro du type générateur de citation (1 ou 2) : "));
-                                                      } // while numeroPhrase
-        
-        // générer nb citations dans un tableau
-        var tableau_citation = [];
-        tableau_citation = generate_citation(nbCitations,numeroPhrase); 
-        // affichage des citations
-        console.log("\nVoici "+ nbCitations +" citation(s) générée(s) avec le type de générateur no: " + numeroPhrase);   
-        var i;
-        for (i = 0; i < nbCitations; i++) { 
-                console.log("Citation "+(i+1)+": "); 
-                cit = tableau_citation [i];
-            	console.log(tableau_citation[i].decrire()); 
- 			}
+       var typeGen = 0;
+       while (typeGen != 1 && typeGen != 2) {
+           typeGen= Number(prompt("Entrer le numéro du type générateur de citation (1 ou 2) : "));
+                                                      } // while typeGen
+        // générer les citations
+        var numPhrase;
+        numPhrase = typeGen - 1;
+        console.log("\nVoici "+ nbCitations +" citation(s) générée(s) avec le type de générateur no: " + typeGen ); 
+        // boucle sur le nb citations
+        for (j = 0; j < nbCitations; j++) { 
+                var phrase1 = "";
+                var position1;
+                        for ( var i = 0 ;i < typeGenerateur[numPhrase].length ; i++) {
+                                    position1 = random (typeGenerateur[numPhrase][i]);    
+                                    phrase1 =  phrase1 + " " + typeGenerateur[numPhrase][i][position1];
+                        }
+                console.log( phrase1);
+        } // nb citations
         break;
         
     default:
           console.log ("Nombre invalide : essayer de nouveau un nombre entre 0 et 5.")
            break;
                     } // switch
-                        
 } // while nbCitations
- 		
+
+
+
